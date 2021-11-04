189ef76b016d2fef8b31439f9cc2432dc8cca475
fourth-homunculus <kyereyes.web@gmail.com>
2021-11-04 23:44:58 +0800

    Build Infrastructure - Initial commit

---
+++ build-infrastructure/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "terraform-aws-get-started"
+  region  = "ap-southeast-1"
+}
+
+resource "aws_instance" "test_server" {
+  ami           = "ami-07191cf2912e097a6"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform - Test Server"
+  }
+}
